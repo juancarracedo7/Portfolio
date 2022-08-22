64437eb9913f9f1981131f528e68da40d90aa959
Juan Cruz Carracedo <juann.carracedo7@gmail.com>
2022-08-22 19:40:17 -0300

    features

--- src/components/certificates/Certificates.js
+++ src/components/certificates/Certificates.js
@@ -6,6 +6,7 @@ import utn from '../../assets/UTN3.png'
 export default function Certificate() {
   return (
     <div className="container">
+    <h1 className="h1">Certificates</h1>
       <img className="img" src={certificadoHenry} alt="nop" />
       <img className="img" src={utn} alt="nop" />
     </div>
