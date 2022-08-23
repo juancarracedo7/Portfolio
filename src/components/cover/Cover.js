d521ac4f625adca398eb884a874c8601e178be1a
Juan Cruz Carracedo <juann.carracedo7@gmail.com>
2022-08-23 13:05:36 -0300

    features

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Cover.css";
-import coverVideo from "../../media/coverVideo.mp4";
+import Video from "../../media/Video.mp4";
 import { useTranslation } from "react-i18next";
 
 const Cover = () => {
@@ -8,7 +8,7 @@ const Cover = () => {
   const [t, i18n] = useTranslation("global")
   return (
     <div className="cover-container">
-      <video className="video" src={coverVideo} autoPlay loop muted />
+      <video className="video" src={Video} autoPlay loop muted />
       <h1>Juan Cruz Carracedo</h1>
       <p>{t("developer.mydeveloper")}</p>
     </div>
